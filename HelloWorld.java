8df0a3857027bc3f70773edf3f0fa4e0c045facf
Pratik Prashant Kulkarni <pratikk2201@gmail.com>
2024-05-29 12:43:46 +0530

    First commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("This is from C-DAC ACTS ...");
+        System.out.println("We are in DAC ...");
+        System.out.println("Vasant");
+    }
+}
